8be02963e846e5adda24c43a1a6d7c2ce4d99823
Tiwari <AnandKumar.Tiwari@gartner.com>
2021-06-18 13:32:09 +0530

    Added semantic-ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
+import "./App.css";
 function App() {
   return (
     <div className="App">
-        <h1>Hello World</h1>
+      <h1>Hello World 2</h1>
+      <button class="ui basic button">
+        <i class="icon user"></i>
+        Add Friend
+      </button>
+      <button class="ui labeled icon button">
+        <i class="pause icon"></i>
+        Pause
+      </button>
+      <button class="ui right labeled icon button">
+        <i class="right arrow icon"></i>
+        Next
+      </button>
     </div>
   );
 }
